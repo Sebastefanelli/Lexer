7330f6e52343afeb95d9feaa7ddd4f3e7abd388d
Sebastefanelli <128101200+Sebastefanelli@users.noreply.github.com>
2023-09-28 14:42:04 -0300

    Actualizar automatas.py

--- automatas.py
+++ automatas.py
@@ -1,167 +1,172 @@
-# Todos los automatas reciben un lexema y devuelven:
-#   - FINAL     : si el lexema es aceptado por el automata.
-#   - NO FINAL  : si el lexema no es aceptado pero tiene posibilidad de serlo.
-#   - TRAMPA    : si el lexema nunca va a poder ser aceptado por el automata.
-
-def afd_der_paren(lexema):
-    if lexema == ")":
-        return "FINAL"
-    else:
-        return "TRAMPA"
+from gramatica import VN, VT, P, SD, simbolo_inicial
+from lexer.lexer import lexer
 
 
-def afd_izq_paren(lexema):
-    if lexema == "(":
-        return "FINAL"
-    else:
-        return "TRAMPA"
+def parser(lista):
+    lista.append(("#", "#"))
+    datos_locales = {
+        'tokens': lista,
+        'index': 0,
+        'error': False,
+        'error_t': None,
+    }
 
+    def procesar(cadena):
+        for simbolo in cadena:
+            actual = datos_locales['tokens'][datos_locales['index']][0]
+            datos_locales['error'] = False
+            if simbolo in VT:
+                if simbolo == actual:
+                    datos_locales['index'] += 1
+                else:
+                    datos_locales['error'] = True
+            elif simbolo in VN:
+                procedimiento_PNI(simbolo)
+                if datos_locales['error']:
+                    break
 
-def afd_id(lexema):
-    estados = ['A', 'B', 'T']
-    estados_finales = ['B']
-    estado_actual = 'A'
-    for c in lexema:
-        if estado_actual == 'A' and c.isalpha():
-            estado_actual = 'B'
-        elif estado_actual == 'B' and c.isalnum():
-            estado_actual = 'B'
+    def procedimiento_PNI(simbolo):
+        datos_locales['error'] = False
+        actual = datos_locales['tokens'][datos_locales['index']][0]
+        simbolos_directrices = SD[simbolo]
+        if actual in simbolos_directrices:
+            procesar(simbolos_directrices[actual])
         else:
-            estado_actual = 'T'
-            break
+            datos_locales['error'] = True
 
-    if estado_actual in estados_finales:
-        return "FINAL"
-    else:
-        return "TRAMPA"
+    def principal():
+        procedimiento_PNI(simbolo_inicial)
+        actual = datos_locales['tokens'][datos_locales['index']][0]
+        if actual != '#' or datos_locales['error']:
+            print('la cadena no pertenece al lenguaje ')
+            return False
+        print('la cadena pertence al lenguaje')
 
+        return True
 
-def afd_num(lexema):
-    if lexema.isnumeric():
-        return "FINAL"
-    else:
-        return "TRAMPA"
-
-
-def afd_puntoycoma(lexema):
-    if lexema == ";":
-        return "FINAL"
-    else:
-        return "TRAMPA"
+    return principal()
 
 
-# Esta funcion toma un lexema, una tabla de transicion en forma de un
-# diccionario de python, una lista de estados finales, y un estado inicial.
-# Es usada por los automatas debajo para calcular si un lexema es aceptado o no.
-def automata_por_tabla(lexema, tabla, estados_finales, estado_inicial):
-    estado_actual = estado_inicial
-    for c in lexema:
-        if c in tabla[estado_actual]:
-            estado_actual = tabla[estado_actual][c]
-        else:
-            estado_actual = 'T'
-            break
-    if estado_actual in estados_finales:
-        return "FINAL"
-    elif estado_actual == 'T':
-        return "TRAMPA"
-    else:
-        return "NO FINAL"
+tests = [
+    # test 1
+    '''
+x equal 98234;
+
+si 6 < 7 entonces
+    x equal x / 4
+sino
+    x equal x / 3
+finsi
+''',
+
+    # test 2
+    '''
+func mult(n1; n2)
+    x equal n1 * n2;
+    mostrar x
+finfunc
+''',
+
+    # test 3
+    '''
+leer x;
+
+i equal 0;
+repetir
+    mostrar i;
+    i equal i + 1
+hasta i = x
+''',
+
+    # test 4
+    '''
+23hola+-sifinsi
+''',
+
+    # test 5
+    ''' 
+repetir
+    iequali+1;
+    leerx;
+    x=x*x;
+    mostrarx
+hasta i=33
+''',
+
+    # test 6
+    ''' 
+vmax=0;
+leer y;
+si y>vmax entonces
+    vmax equal y
+sino
+    e equal e+1
+finsi
+mostrar vmax;
+mostrar e
+''',
+
+    # test 7
+    '''
+repetir
+    i equal i+1;
+    leer nombre;
+    leer edad;
+    si edad>=18 entonces 
+        mostrar nombre
+    sino
+        vdif equal 18-edad;
+        mostrar vdif
+    finsi
+hasta i=20
+''',
+
+    # test 8
+    '''
+leer x;
+leer y;
+si x>y entonces 
+    x equal x+y
+sino
+    y equal x+y
+finsi
+''',
+
+    # test 9
+    '''
+func rest(n1; n2)
+    x equal n1 - n2;
+    mostrar x
+finfunc
+''',
+
+    # test 10
+    '''
+vmax equal 0;
+repetir
+    i equal i+1;
+    leer socio;
+    leer dni;
+    leer edad;
+    si edad>vmax entonces 
+        msocio equal socio;
+        mdni equal dni;
+        medad equal edad
+    finsi
+hasta i=50;
+mostrar msocio;
+mostrar mdni;
+mostrar medad
+''',
+]
 
 
-def afd_entonces(lexema):
-    tabla = {
-        'A': {'e': 'B'},
-        'B': {'n': 'C'},
-        'C': {'t': 'D'},
-        'D': {'o': 'E'},
-        'E': {'n': 'F'},
-        'F': {'c': 'G'},
-        'G': {'e': 'H'},
-        'H': {'s': 'I'},
-        'I': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["I"], "A")
-
-
-def afd_equal(lexema):
-    tabla = {
-        'A': {'e': 'B'},
-        'B': {'q': 'C'},
-        'C': {'u': 'D'},
-        'D': {'a': 'E'},
-        'E': {'l': 'F'},
-        'F': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["F"], "A")
-
-
-def afd_finfunc(lexema):
-    tabla = {
-        'A': {'f': 'B'},
-        'B': {'i': 'C'},
-        'C': {'n': 'D'},
-        'D': {'f': 'E'},
-        'E': {'u': 'F'},
-        'F': {'n': 'G'},
-        'G': {'c': 'H'},
-        'H': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["H"], "A")
-
-
-def afd_finsi(lexema):
-    tabla = {
-        'A': {'f': 'B'},
-        'B': {'i': 'C'},
-        'C': {'n': 'D'},
-        'D': {'s': 'E'},
-        'E': {'i': 'F'},
-        'F': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["F"], "A")
-
-
-def afd_func(lexema):
-    tabla = {
-        'A': {'f': 'B'},
-        'B': {'u': 'C'},
-        'C': {'n': 'D'},
-        'D': {'c': 'E'},
-        'E': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["E"], "A")
-
-
-def afd_hasta(lexema):
-    tabla = {
-        'A': {'h': 'B'},
-        'B': {'a': 'C'},
-        'C': {'s': 'D'},
-        'D': {'t': 'E'},
-        'E': {'a': 'F'},
-        'F': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["F"], "A")
-
-
-def afd_leer(lexema):
-    tabla = {
-        'A': {'l': 'B'},
-        'B': {'e': 'C'},
-        'C': {'e': 'D'},
-        'D': {'r': 'E'},
-        'E': {},
-        'T': {}
-    }
-    return automata_por_tabla(lexema, tabla, ["E"], "A")
-
+for i in range(len(tests)):
+    print(f'=========TEST N°{i+1}=========')
+    print(tests[i])
+    print(f'---- RESULTADO \t {parser(lexer(tests[i]))}')
+    print("")
+    print("")
 
 def afd_mostrar(lexema):
     tabla = {
